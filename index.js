73790c9cf5efae53a2cf28ea0456fd8387566e80
Aman <amsingh714@gmail.com>
2019-04-01 13:32:40 -0400

    able to get users

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ server.post('/api/register', (req, res) => {
   let user = req.body;
 
   // hash the password 
-  const hash = bcrypt.hashSync(user.password, 8); // number of rounds
+  const hash = bcrypt.hashSync(user.password, 8); // number of rounds (2 ^ 8)
 
   // store the hash back into the user.password.
   user.password = hash;
-
 
   Users.add(user)
     .then(saved => {
@@ -42,7 +41,7 @@ server.post('/api/login', (req, res) => {
   Users.findBy({ username })
     .first()
     .then(user => {
-      if (user) {
+      if (user && bcrypt.compareSync(password, user.password)) {
         res.status(200).json({ message: `Welcome ${user.username}!` });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -53,13 +52,36 @@ server.post('/api/login', (req, res) => {
     });
 });
 
-server.get('/api/users', (req, res) => {
+server.get('/api/users', restricted, (req, res) => {
   Users.find()
     .then(users => {
       res.json(users);
     })
     .catch(err => res.send(err));
 });
+
+function restricted(req, res, next) {
+  const { username, password } = req.headers;
+  
+  if (username && password) {
+    Users.findBy({ username })
+    .first()
+    .then(user => {
+      // check that passwords match 
+      if (user && bcrypt.compareSync(password, user.password)) {
+        next()
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({message: "Ran into an unexpected error" })
+    }) 
+  } else {
+    res.status(400).json({ message: "No credentials provided" })
+  }
+}
+
 
 const port = process.env.PORT || 5000;
 server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
